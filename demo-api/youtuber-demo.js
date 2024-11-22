626bddbc1f776a4931b7280b13604efc52d4b9b0
asdfbn12345 <asdfbn12345@gmail.com>
2024-11-23 02:04:16 +0900

    express delete

--- demo-api/youtuber-demo.js
+++ demo-api/youtuber-demo.js
@@ -4,13 +4,13 @@ const app = express();
 const port = 3000;
 
 let id = 1;
-const youTubers = new Map();
+const youtubers = new Map();
 
-youTubers.set(id++, new YouTuber("게임-유튜버", 1256624, 135));
+youtubers.set(id++, new YouTuber("게임-유튜버", 1256624, 135));
 
-youTubers.set(id++, new YouTuber("개발-유튜버", 426624, 53));
+youtubers.set(id++, new YouTuber("개발-유튜버", 426624, 53));
 
-youTubers.set(id++, new YouTuber("운동-유튜버", 212058, 81));
+youtubers.set(id++, new YouTuber("운동-유튜버", 212058, 81));
 
 app.use(express.json());
 
@@ -19,8 +19,8 @@ app.listen(port);
 let allYoutubers = [];
 
 app.get("/youtubers", (req, res) => {
-  youTubers.forEach((youTuber) => {
-    allYoutubers.push(youTuber);
+  youtubers.forEach((youtuber) => {
+    allYoutubers.push(youtuber);
   });
 
   res.json(allYoutubers);
@@ -28,21 +28,45 @@ app.get("/youtubers", (req, res) => {
 
 app.get("/youtubers/:id", (req, res) => {
   const { id } = req.params;
-  let youTuber = youTubers.get(parseInt(id));
+  let youtuber = youtubers.get(parseInt(id));
 
-  if (youTuber == undefined) {
+  if (youtuber == undefined) {
     res.json({
       message: "등록되지 않은 유튜버입니다.",
     });
   } else {
-    res.json(youTuber);
+    res.json(youtuber);
   }
 });
 
 app.post("/youtubers", (req, res) => {
   const { channelTitle, subscriber, videoCount } = req.body;
 
-  youTubers.set(id++, new YouTuber(channelTitle, subscriber, videoCount));
+  youtubers.set(id++, new YouTuber(channelTitle, subscriber, videoCount));
 
   res.json(`Welcome ${channelTitle}!`);
+});
+
+app.delete("/youtubers", (req, res) => {
+  res.json({
+    message: "너무 위험한 요청입니다.",
+  });
+});
+
+app.delete("/youtubers/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  let youtuber = youtubers.get(id);
+  if (!youtuber) {
+    res.json({
+      message: "유튜버 ID가 등록되어있지 않습니다.",
+    });
+    return;
+  }
+
+  channelTitle = youtuber.channelTitle;
+  youtubers.delete(id);
+  res.json({
+    message: `${channelTitle}님 이용해주셔서 감사합니다.`,
+  });
 });
